68f3aacc62ccd48331b11a12488c41923093cb63
Alpharius <fury07.01.01@gmail.com>
2024-06-21 05:18:24 +0500

    Добавьте файлы проекта.

---
+++ ProjectMovie/Data/ProjectMovieContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjectMovie.Models;
+
+namespace ProjectMovie.Data
+{
+    public class ProjectMovieContext : DbContext
+    {
+        public ProjectMovieContext (DbContextOptions<ProjectMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjectMovie.Models.Movie> Movie { get; set; } = default!;
+    }
+}
